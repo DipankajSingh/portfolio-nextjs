092f05a5fba8cd68c22c6bb1e1e41eeb4d89c0d5
Dipankaj Singh <dipankajsingh2@.com>
2024-05-23 13:20:51 +0530

    upadates

--- app/components/server/SmallProjects.jsx
+++ app/components/server/SmallProjects.jsx
@@ -1,8 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
-export default function SmallProjects() {
+const githubRequestConfig = {
+    method: "GET",
+    headers: {
+        "Authorization": `token ${process.env.GITHUB_API_KEY}`
+    }
+}
+
+async function getRepoCount() {
+
+    const response = await fetch("https://api.github.com/users/DipankajSingh/repos", githubRequestConfig);
+    const data = await response.json();
+    return data.length
+}
+
+export default async function SmallProjects() {
     return (
-        <section className="allProjects" id="allProjects">
+        <section className=" relative mr-auto ml-auto text-2xl h-max font-inter mt-8 text-center" >
+            Please visit my github profile to see all {await getRepoCount()} projects <Link className="inline " href="https://github.com/DipankajSingh"><Image className='filter-greeen inline' src="/githubIcon.png" alt="github" width={50} height={50} /></Link>
+
 
         </section>
     )
